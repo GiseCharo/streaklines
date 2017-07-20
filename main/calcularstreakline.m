c1c740a86b9da33119e5b4ef50122d98ae6ebdef
Valentin Resseguier <valentin@niederwind.local>
2017-07-20 16:06:13 -0300

    Write the different possiblities for the velocity field in the script which plots the streklines

--- main/calcularstreakline.m
+++ main/calcularstreakline.m
@@ -4,10 +4,14 @@ clear;close all
 %%deltat0 is the time interval of injection,
 x0(1,1)=1;x0(1,2)=0.5;
 t0=0;deltat0=0.01;t=5;
-vel='couetteplanNS';
+% vel='couetteplanS';
+%vel='couetteplanNS';
+% vel='DGyreS';
+vel='DGyreNS';
 streakline1=streakline(x0,t0,deltat0,t,vel);
 
 figure()
 plot(streakline1(:,1),streakline1(:,2),'o-')
+axis xy;axis equal
 %title('streakline, P=(1,0.5), deltat0=0.01, t=30')
 %axis([0 2 0 1])
