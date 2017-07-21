7764968d1dca9481cd1d41cabb018b86e1126b9e
vressegu <valentin.resseguier@gmail.com>
2017-07-21 15:22:13 -0300

    more available options for plots in phi_grid and for the new velocity field fct_wake_megaRAM

--- main/Advection_grid.m
+++ main/Advection_grid.m
@@ -8,6 +8,8 @@ init
 % vel=@couetteplanS;
 % x=linspace(0,2,2*N);
 % y=linspace(0,1,N);
+% T_plot = 30;
+% n_sub = 3;
 
 % % Non-stationnary Couette-plan
 % t0=0;t=50;
@@ -15,6 +17,8 @@ init
 % vel=@couetteplanNS;
 % x=linspace(0,2,2*N);
 % y=linspace(0,1,N);
+% T_plot = 30;
+% n_sub = 3;
 
 % % Stationnary double gyre
 % t0=0;t=50;
@@ -22,6 +26,8 @@ init
 % vel=@DGyreS;
 % x=linspace(0,2,2*N);
 % y=linspace(0,1,N);
+% T_plot = 10;
+% n_sub = 1;
 
 % % Non-stationnary double gyre
 % t0=0;t=50;
@@ -29,16 +35,29 @@ init
 % vel=@DGyreNS;
 % x=linspace(0,2,2*N);
 % y=linspace(0,1,N);
+% T_plot = 10;
+% n_sub = 1;
 
-% Wake flow
-t0=0;t=40;
+% % Wake flow
+% t0=0;t=10;
+% N= 120;
+% vel=@fct_wake;
+% x=linspace(0,20,2*N);
+% y=linspace(-6,6,N);
+% T_plot = 100;
+% n_sub = 3;
+
+% Wake flow MEGA RAM
+t0=0;t=10;
 N= 120;
-vel=@fct_wake;
+vel=@fct_wake_megaRAM;
 x=linspace(0,20,2*N);
 y=linspace(-6,6,N);
+T_plot = 100;
+n_sub = 3;
 
 % Grid
 [X,Y]=ndgrid(x,y);
 
 % Lagrangian advection
-[X,Y] = phi_grid(X,Y,t0,t,vel,true);
+[X,Y] = phi_grid(X,Y,t0,t,vel,true,n_sub,T_plot );
