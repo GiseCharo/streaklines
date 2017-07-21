8dfe16470fffa651e63624cb7e5e3ad2aa1a5e0a
vressegu <valentin.resseguier@gmail.com>
2017-07-20 23:16:37 -0300

    increment in the loop

--- functions/flow/streakline.m
+++ functions/flow/streakline.m
@@ -3,6 +3,6 @@ function streakl=streakline(x0,t0,deltat0,t,velocity)
 time0=t0:deltat0:t;
 streakl=zeros(length(time0),2);
 for i=1:length(time0)
-streakl(i,:)=phi(x0,time0(i),t,velocity);
+    streakl(i,:)=phi(x0,time0(i),t,velocity);
 end
 end
