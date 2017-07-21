19657ba340b4c0525da7eb5a847e00d89513231c
GiseCharo <charogiselad@gmail.com>
2017-07-21 20:09:11 -0300

    and old function was deleted

--- functions/flow/dtideltatphi.m
+++
@@ -1,7 +0,0 @@
-function dtideltatphi=dtideltatphi(x0,t0,deltat0,t,i,velocity)
-%%%calcula la derivada de phi tilde en t0
-sol2=phi(x0,t0+deltat0,t0+deltat0+t-(i+1)*deltat0,velocity);
-sol1=phi(x0,t0,t0+t-(i+1)*deltat0,velocity);
-
-dtideltatphi=(sol2-sol1)/deltat0;
-end
