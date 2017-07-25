65abe0c65fe7a134c8a1d1d73ef80ad48e269113
vressegu <valentin.resseguier@gmail.com>
2017-07-25 19:20:45 -0300

    interpolation along the streakline for any ND-array field

---
+++ functions/flow/interpstreak_any.m
@@ -0,0 +1,20 @@
+function Hstreak=interpstreak_any(stline,H,X,Y,t0)
+%%interpolates the H along the streakline
+%
+
+% Reshape
+s = size(H);
+N = prod(s(3:end));
+H = reshape(H,[s(1:2) N]);
+
+% Initialisation
+Hstreak=zeros([length(t0),N]);
+for k=1:N % Loop along the streakline
+    Hstreak(:,k) =interp2(X(:,1),Y(1,:),H(:,:,k)',stline(:,1),stline(:,2));
+end
+
+% Reshape
+if length(s)<3
+    s(3)=1;
+end
+Hstreak = reshape(Hstreak,[length(t0) s(3:end)]);
