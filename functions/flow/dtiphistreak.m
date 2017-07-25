7bdbd0bcd88056a827f2c3088732a6566b4e0d6d
vressegu <valentin.resseguier@gmail.com>
2017-07-25 19:24:45 -0300

    See last comment

--- functions/flow/dtiphistreak.m
+++ functions/flow/dtiphistreak.m
@@ -1,10 +1,12 @@
 function Dtiphi=dtiphistreak(t0,x0,deltat0,t,velocity)
 %%computes the correction of the tangent along the streakline
 Dtiphi=zeros(length(t0),2);
-for j=1:length(t0)-1
+for j=1:length(t0)
     Dtiphi(j,:)=dtiphi(x0,t0(j),deltat0,t-t0(j),velocity);
 end
-Dtiphi(end,:)=(phi(x0,t0(end),t0(end)+t-t0(end),velocity)-phi(x0,t0(end-1)...
-    ,t0(end-1)+t-t0(end-1),velocity))/deltat0;
+% Dtiphi(end,:)=(phi(x0,t0(end),t,velocity)-phi(x0,t0(end-1)...
+%     ,t,velocity))/deltat0;
+% % Dtiphi(end,:)=(phi(x0,t0(end),t0(end)+t-t0(end),velocity)-phi(x0,t0(end-1)...
+% %     ,t0(end-1)+t-t0(end-1),velocity))/deltat0;
 end
 
