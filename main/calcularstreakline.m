87da040e60470fb5e881f4e26856b9b3774a19fa
vressegu <valentin.resseguier@gmail.com>
2017-07-26 13:53:06 -0300

    not important modifications

--- main/calcularstreakline.m
+++ main/calcularstreakline.m
@@ -2,13 +2,18 @@ clear;close all
 %%computes the streakline
 %%x0 is the point of injection,t0 the first time of injection and t the last,
 %%deltat0 is the time interval of injection,
-x0(1,1)=1;x0(1,2)=0.5;
-t0=0;deltat0=0.01;t=5;
+
+% x0(1,1)=1;x0(1,2)=0.5;
+%t0=0;deltat0=0.01;t=5;
 % vel=@couetteplanS;
 % vel=@DGyreS;
 % vel=@DGyreNS;
-vel=@couetteplanNS;
-vel=@DGyreNS;
+% vel=@couetteplanNS;
+
+x0(1,1)=6;x0(1,2)=0;
+t0=0;deltat0=0.1;t=10;
+vel=@fct_wake;
+
 streakline1=streakline(x0,t0,deltat0,t,vel);
 
 figure()
