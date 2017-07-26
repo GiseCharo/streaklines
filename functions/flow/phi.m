932f95f76ba8cb7c8119fa9811e71c13dd90c877
vressegu <valentin.resseguier@gmail.com>
2017-07-26 13:49:22 -0300

    Prevent very long integration of the flow due to wrong choice in function phi.m. t is sometimes believed to be larger than t_0 due to numerical error. We now use a numerical zero.

--- functions/flow/phi.m
+++ functions/flow/phi.m
@@ -4,11 +4,18 @@ function  phi=phi(p0,t0,t,velocity)
 
 velocity=@(t,CIx) velocity_on_pt(velocity,t,CIx);
 options = odeset('RelTol',1e-8,'AbsTol',1e-10);
-if t>t0
-    [T,sol]=ode45(velocity, [t0 t],p0,options);
+% %if t ~= t0
+% if t>t0
+%     [~,sol]=ode45(velocity, [t0 t],p0,options);
+%     phi=sol(end,:);
+% elseif t==t0
+%     phi=p0;
+if t>t0+10*eps
+    [~,sol]=ode45(velocity, [t0 t],p0,options);
     phi=sol(end,:);
-elseif t==t0
+elseif abs(t-t0)<10*eps
     phi=p0;
+else
+    error('Backward advection not encoded yet');
 end
-
 end
