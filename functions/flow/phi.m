bed157333aa63037d2a776454c3adc2a48834810
vressegu <valentin.resseguier@gmail.com>
2017-07-28 14:07:29 -0300

    smaller tolerance for ode45 in order to have a faster algorithm

--- functions/flow/phi.m
+++ functions/flow/phi.m
@@ -3,7 +3,7 @@ function  phi=phi(p0,t0,t,velocity)
 %
 
 velocity=@(t,CIx) velocity_on_pt(velocity,t,CIx);
-options = odeset('RelTol',1e-8,'AbsTol',1e-10);
+options = odeset('RelTol',1e-6,'AbsTol',1e-8);
 % %if t ~= t0
 % if t>t0
 %     [~,sol]=ode45(velocity, [t0 t],p0,options);
