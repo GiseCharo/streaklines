dbfe83e633ea00b1df29d6fd406be6446e362cce
vressegu <valentin.resseguier@gmail.com>
2017-07-28 15:05:57 -0300

    Remove useless argument in interpstreak_any.m and dt0phistreak.m. WARNING: it may create bugs (easy to correct in current scripts.

--- functions/flow/dt0phistreak.m
+++ functions/flow/dt0phistreak.m
@@ -1,6 +1,7 @@
-function Dt0phi=dt0phistreak(t0,x0,deltat0,t,velocity)
+function Dt0phi=dt0phistreak(t0,x0,t,velocity)
 %%computes the the tangent along the streakline
 Dt0phi=zeros(length(t0),2);
+deltat0 = t0(2)-t0(1);
 for j=1:length(t0)
     Dt0phi(j,:)=dtophi(x0,t0(j),t,deltat0,velocity);
 end
--- functions/flow/interpstreak_any.m
+++ functions/flow/interpstreak_any.m
@@ -1,4 +1,4 @@
-function Hstreak=interpstreak_any(stline,H,X,Y,t0)
+function Hstreak=interpstreak_any(stline,H,X,Y)
 %%interpolates the H along the streakline
 %
 
@@ -8,7 +8,7 @@ N = prod(s(3:end));
 H = reshape(H,[s(1:2) N]);
 
 % Initialisation
-Hstreak=zeros([length(t0),N]);
+Hstreak=zeros([size(stline,1),N]);
 for k=1:N % Loop along the streakline
     Hstreak(:,k) =interp2(X(:,1),Y(1,:),H(:,:,k)',stline(:,1),stline(:,2));
 end
@@ -17,4 +17,4 @@ end
 if length(s)<3
     s(3)=1;
 end
-Hstreak = reshape(Hstreak,[length(t0) s(3:end)]);
+Hstreak = reshape(Hstreak,[size(stline,1) s(3:end)]);
