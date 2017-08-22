2385cce413332b44683d2ba6dc103b968568a5c5
vressegu <valentin.resseguier@gmail.com>
2017-08-22 18:33:04 -0300

    Script main/plot_on_streakline_spacially.m in order to compare the topologic mixing criterion with a criterion based on the streakline tangente norm

--- functions/flow/dt0phistreak.m
+++ functions/flow/dt0phistreak.m
@@ -1,7 +1,9 @@
-function Dt0phi=dt0phistreak(t0,x0,t,velocity)
+function Dt0phi=dt0phistreak(t0,x0,t,velocity,deltat0)
 %%computes the the tangent along the streakline
 Dt0phi=zeros(length(t0),2);
-deltat0 = t0(2)-t0(1);
+if nargin < 5
+    deltat0 = t0(2)-t0(1);
+end
 for j=1:length(t0)
     Dt0phi(j,:)=dtophi(x0,t0(j),t,deltat0,velocity);
 end
